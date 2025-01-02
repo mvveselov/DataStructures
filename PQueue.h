00b4bb99ac08ff782e3f27fc4564c1b9949a54e5
mvveselov <mvveselov@bk.ru>
2025-01-02 14:27:07 +0700

    Class Point added to the Project. For farther usage with different Projects class Point located in the separate directory

--- PQueue.h
+++ PQueue.h
@@ -2,32 +2,49 @@
 #include <iostream>
 using namespace std;
 
-template <int size>
+template <class T, int capacity>
 class PQueue {
-	int arr[size]{};
+	T arr[capacity]{};
 	int n;
 public:
 	PQueue() { n = 0; }
-	void enqueue (int e) {
-		if (n == size) {
-			if(arr[n - 1] < e)
-				return;
-		} else 
-			n++;
-
-		int i{};
-		i = n - 2;
-
-		while (i >= 0 && e < arr[i]) {
-			arr[i + 1] = arr[i];
-			i--;
-		}
-
-		arr[i+1] = e;
-	}
+	void enqueue(T e);
+	void dequeue();
+	const int front() const { return *arr; }
 	void print() {
 		for (int i = 0; i < n; i++)
 			cout << arr[i] << ' ';
 		cout << endl;
 	}
+	int size() { return n; }
+
 };
+
+template <class T, int capacity>
+void PQueue<T, capacity>::enqueue(T e) {
+	if (n == capacity) {
+		if (arr[n - 1] < e)
+			return;
+	}
+	else
+		n++;
+
+	int i{};
+	i = n - 2;
+
+	while (i >= 0 && e < arr[i]) {
+		arr[i + 1] = arr[i];
+		i--;
+	}
+
+	arr[i + 1] = e;
+}
+
+template <class T, int capacity>
+void PQueue<T, capacity>::dequeue() {
+	if (n == 0)
+		return;
+	for (int i = 0; i < n - 1; i++)
+		arr[i] = arr[i + 1];
+	n--;
+}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
 #include "PQueue.h"
+#include "C:\\Users\\vesel\\source\\repos\\classes\\point\\Point.h"
 using namespace std;
 
 
 
 void main() {
 	
-	PQueue<4> pqueue;
-	pqueue.enqueue(10);
-	pqueue.enqueue(5);
-	pqueue.enqueue(4);
+	PQueue<int, 4> pq;
+	Point p{ 1,2 };
+	cout << p << endl;
 
-	pqueue.enqueue(2);
-	pqueue.enqueue(1);
-	pqueue.enqueue(3);
-	pqueue.enqueue(1);
-	pqueue.enqueue(2);
-	pqueue.enqueue(10);
-	////look up first pages of data structures
-	////Highest priority should push lower one out
-
-	pqueue.print();
-	
 }
